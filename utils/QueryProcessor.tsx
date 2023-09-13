8914e43a8bf9289c1c3b4a41fb810de94d074a40
Swati Anshu <swatianshu27@gmail.com>
2023-09-13 14:36:47 -0400

    added Andrew ID query

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -1,11 +1,15 @@
 export default function QueryProcessor(query: string): string {
+  if (query.toLowerCase().includes("andrew")){
+    return (
+      "Andrew ID is your cmu ID"
+    );
+  }
   if (query.toLowerCase().includes("shakespeare")) {
     return (
       "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
       "English poet, playwright, and actor, widely regarded as the greatest " +
       "writer in the English language and the world's pre-eminent dramatist."
-    );
+    ); 
   }
-
   return "";
 }
