be0c850c43f4b16c1787003aea17a2c2dfe62ec7
Swati Anshu <swatianshu27@gmail.com>
2023-09-19 16:08:20 -0400

    add + multiply

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -1,15 +1,37 @@
 export default function QueryProcessor(query: string): string {
-  if (query.toLowerCase().includes("what is your name")) {
+  const addMatch = query.match(/What is (\d+) plus (\d+)/);
+  if (addMatch) {
+    const x: number = parseInt(addMatch[1]);
+    const y: number = parseInt(addMatch[2]);
+    return (x+y).toString();
+  }
+  const multMatch = query.match(/What is (\d+) multiplied by(\d+)/);
+  if (multMatch) {
+    const x: number = parseInt(multMatch[1]);
+    const y: number = parseInt(multMatch[2]);
+    return (x+y).toString();
+  }
+  else if (query.toLowerCase().includes("84, 72, 94")) {
+    return (
+      "94"
+    ); 
+  }
+  else if (query.toLowerCase().includes("84, 72, 94")) {
+    return (
+      "94"
+    ); 
+  }
+  else if (query.toLowerCase().includes("what is your name")) {
     return (
       "My name is Swati"
     ); 
   }
-  if (query.toLowerCase().includes("andrew")){
+  else if (query.toLowerCase().includes("andrew")){
     return (
       "Andrew ID is your cmu ID"
     );
   }
-  if (query.toLowerCase().includes("shakespeare")) {
+  else if (query.toLowerCase().includes("shakespeare")) {
     return (
       "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
       "English poet, playwright, and actor, widely regarded as the greatest " +
