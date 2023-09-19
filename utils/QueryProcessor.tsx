088cfba7b27fa016f169c59a8a15bf786b5dde14
Swati Anshu <swatianshu27@gmail.com>
2023-09-19 16:29:23 -0400

    Check for cube and square and minus

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -1,45 +1,8 @@
 export default function QueryProcessor(query: string): string {
-  var isSquare = function (n : number) {
+  var isSquare = function (n : number)  {
     return Math.sqrt(n) % 1 === 0;
 };
 
-function isCubeRoot(x: number) {    
-  if (x === 0) {
-      return 0;
-  }
-  if (x < 0) {
-      return -isCubeRoot(-x);
-  }
-
-  var r = x;
-  var ex = 0;
-
-  while (r < 0.125) { 
-      r *= 8; ex--; 
-  }
-  while (r > 1.0) { 
-      r *= 0.125; ex++; 
-  }
-
-  r = (-0.46946116 * r + 1.072302) * r + 0.3812513;
-
-  while (ex < 0) { 
-      r *= 0.5; ex++; 
-  }
-  while (ex > 0) { 
-      r *= 2; ex--; 
-  }
-
-  r = (2.0 / 3.0) * r + (1.0 / 3.0) * x / (r * r);
-  r = (2.0 / 3.0) * r + (1.0 / 3.0) * x / (r * r);
-  r = (2.0 / 3.0) * r + (1.0 / 3.0) * x / (r * r);
-  r = (2.0 / 3.0) * r + (1.0 / 3.0) * x / (r * r);
-
-  if (Number.isInteger(r)) {
-      return r;
-  }
-  return false;
-}
   const addMatch = query.match(/What is (\d+) plus (\d+)/);
   const multMatch = query.match(/What is (\d+) multiplied by (\d+)/);
   const minusMatch = query.match(/What is (\d+) minus (\d+)/);
@@ -49,13 +12,13 @@ function isCubeRoot(x: number) {
     const b: number = parseInt(sqroMatch[2])
     const c: number = parseInt(sqroMatch[3])
     const d: number = parseInt(sqroMatch[4])
-    if (isSquare(a) && isCubeRoot(a))
+    if (isSquare(a) && Number.isInteger(a))
        return (a.toString()); 
-    if (isSquare(b) && isCubeRoot(b))
+    if (isSquare(b) && Number.isInteger(b))
        return (b.toString()); 
-    if (isSquare(c) && isCubeRoot(c))
+    if (isSquare(c) && Number.isInteger(c))
        return (c.toString()); 
-    if (isSquare(d) && isCubeRoot(d))
+    if (isSquare(d) && Number.isInteger(d))
        return (d.toString()); 
   }
   if (addMatch) {
